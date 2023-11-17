a435051aff27807fc8038912e7279bf45da01955
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-11-17 21:27:37 +0400

    CSS 80% complete

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,7 +1,24 @@
-import "./Authentication.module.css"
+import { useState } from "react"
+import styles from "./Authentication.module.css"
 
 const Authentication = () => {
-  return <div></div>
+  const [email, setEmail] = useState("")
+  const [pass, setPass] = useState("")
+
+  return (
+    <div className={styles.main}>
+      <form className={styles.form}>
+        <h1 className={styles.header}>Sign up</h1>
+        <input type="email" name="" value={email} placeholder="Email" />
+        <input type="password" name="" value={pass} placeholder="Password" />
+        <label className={styles.checkbox}>
+          <input type="checkbox" name="" value="" />
+          <span>I agree with non-existent rules</span>
+        </label>
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  )
 }
 
 export default Authentication
