546f0a07a6be225e5446a34a245ef78268958259
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-11-18 16:00:05 +0400

    Added logic in this project

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,21 +1,83 @@
 import { useState } from "react"
+import fetchMock from "fetch-mock"
 import styles from "./Authentication.module.css"
 
 const Authentication = () => {
   const [email, setEmail] = useState("")
-  const [pass, setPass] = useState("")
+  const [password, setPassword] = useState("")
+  const [check, setCheck] = useState(false)
+  const [response, setResponse] = useState(null)
+
+  const EMAIL = "example@mail.ru"
+  const PASSWORD = "Example2023"
+
+  async function handleSubmit(el) {
+    el.preventDefault()
+
+    const data = { email, password }
+
+    try {
+      if (email === "") return alert("You missed Email field!")
+      if (password === "") return alert("You missed Password field!")
+      if (check === false)
+        return alert("You do not agree with non-existent rules!")
+
+      if (email === EMAIL && password === PASSWORD) {
+        fetchMock.mock("https://example.com", {
+          body: { yourEmail: email, yourPassword: password },
+          status: 200,
+        })
+
+        const response = await fetch("https://example.com", {
+          method: "POST",
+          body: JSON.stringify(data),
+        })
+        if (!response.ok) throw new Error("Network response was not ok.")
+
+        const responseData = await response.json()
+        setResponse(responseData)
+        return alert(
+          `All good! You are logged in.\nYour Email: \n${email} \nand your Password is \n${password}`
+        )
+      } else {
+        return alert("Email or Password is incorrect")
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      fetchMock.restore()
+    }
+  }
+
+  console.log(JSON.stringify(response))
 
   return (
     <div className={styles.main}>
       <form className={styles.form}>
         <h1 className={styles.header}>Sign up</h1>
-        <input type="email" name="" value={email} placeholder="Email" />
-        <input type="password" name="" value={pass} placeholder="Password" />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
         <label className={styles.checkbox}>
-          <input type="checkbox" name="" value="" />
+          <input
+            type="checkbox"
+            checked={check}
+            onChange={(e) => setCheck(e.target.checked)}
+          />
           <span>I agree with non-existent rules</span>
         </label>
-        <button type="submit">Log in</button>
+        <button onClick={handleSubmit} type="submit">
+          Log in
+        </button>
       </form>
     </div>
   )
